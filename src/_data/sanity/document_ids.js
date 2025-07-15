37b3823f187466fa64db4976ffdd820e0b79d417
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2025-07-15 08:56:16 -0700

    update templates to reflect Sanity workspaces

---
+++ src/_data/sanity/document_ids.js
@@ -0,0 +1,7 @@
+/* Sanity doc ids for easy reference */
+
+export default {
+    home: "b451e470-6379-42fa-839e-3cd3c2850996",
+    contact: "0e287de1-042a-4ed5-9322-89a2a64ccbcb",
+    mainNav: "9bc06c89-9fbf-4d77-878d-76da31b376b4",
+};
---
+++ src/lib/utils/workspaceTag.js
@@ -0,0 +1,5 @@
+export const workspaceTag = {
+    id: "cd4015cd-262c-44d1-a1d2-27bf6d0d5b1f",
+    slug: "coliving",
+    title: "Co-Living Sub-Brand",
+};
